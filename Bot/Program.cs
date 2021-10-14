cebc3d3e1f19f772b498ccbb6df765d2b4767487
Nik Volkov <n_volkov@head-point.ru>
2021-10-14 14:34:49 +0300

    Init commit

---
+++ Bot/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading.Tasks;
+using Telegram.Bot;
+
+namespace Bot
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var botClient = new TelegramBotClient("");
+            var me = await botClient.GetMeAsync();
+            Console.WriteLine(
+              $"Hello, World! I am user {me.Id} and my name is {me.FirstName}."
+            );
+        }
+    }
+}
